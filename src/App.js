24def91c19c73710a324d1a52f57c30a667bb822
Laurent De Marez <laurent@demarez.org>
2019-08-28 13:01:25 +0200

    Add react-helmet and OG tags

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import {Helmet} from 'react-helmet';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [user, setUser] = useState({})
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      const response = await fetch('https://randomuser.me/api/');
+      const userResult = await response.json()
+      setUser(userResult.results[0]);
+    }
+    fetchUser();
+  }, []);
+  console.log(user);
   return (
     <div className="App">
+      <Helmet>
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content={window.location.href} />
+        <meta property="og:title" content={user && user.email} />
+        <meta property="og:image" content={user && user.picture && user.picture.thumbnail} />
+      </Helmet>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
